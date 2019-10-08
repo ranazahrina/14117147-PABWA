3e5808afc8787a65125875a7fdd771ab46c7e76a
ranadzahrina <50402481+ranadzahrina@users.noreply.github.com>
2019-10-08 22:31:38 +0700

    Latihan Minggu-6

---
+++ Minggu-6/1_1.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Contoh Skrip PHP</title>
+  </head>
+  <body>
+    <?php
+    $nama = "Kim taehyung";
+    echo "$nama berkata \"save me save me\" "
+    ?>
+  </body>
+</html>
---
+++ Minggu-6/mengurutkan.php
@@ -0,0 +1,9 @@
+<?php
+$nama = ["lanirne","aduh","qifuat","toda","anevi","samid","kifuat"];
+
+echo "Data belum urut : "; echo "($nama[0], $nama[1], $nama[2], $nama[3], $nama[4], $nama[5], $nama[6])";
+echo "<br/>";
+echo "Data sudah urut : ";
+sort($nama);
+echo "($nama[0], $nama[1], $nama[2], $nama[3], $nama[4], $nama[5], $nama[6])";
+ ?>
